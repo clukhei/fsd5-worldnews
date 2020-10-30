57146bc2779dc3a70107f537bf2a5211fdb27436
Lu Khei <kheiboardwarrior@gmail.com>
2020-10-30 15:50:10 +0800

    caching on browser, WIP to change to caching on server

--- main.js
+++ main.js
@@ -9,42 +9,88 @@ const app = express();
 const PORT =
 	parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
 const NEWS_URL = "https://newsapi.org/v2/top-headlines";
-
+const headers = {
+	"X-Api-Key": `${process.env.API_KEY}`,
+};
 app.use(express.static(__dirname + "/static"));
 app.engine("hbs", hbs({ defaultLayout: "default.hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", async (req, res) => {
+	const search = [];
 	res.status(200);
 	res.type("text/html");
-	res.render("index");
+	res.render("index", { searchState: JSON.stringify(search) });
 });
 
 app.post(
 	"/submit",
 	express.urlencoded({ extended: true }),
 	express.json(),
-	async (req, res) => {
-		const fetchUrl = withQuery(NEWS_URL, {
-			q: req.body.search,
-			apiKey: `${process.env.API_KEY}`,
-			country: req.body.country,
-			category: req.body.category,
+	(req, res, next) => {
+		/*        console.log(req.body.searchState) */
+		const prevSearches = JSON.parse(req.body.searchState);
+		console.log(prevSearches);
+		const filter = prevSearches.filter((s) => {
+			if (
+				s.q == req.body.search &&
+				s.country == req.body.country &&
+				s.category == req.body.category
+			) {
+				return s;
+			}
 		});
 
-		try {
-			const result = await fetch(fetchUrl);
-			const data = await result.json();
-			console.log(result);
-			console.log(data);
-            console.log(data.totalResults);
-            console.log(data.articles)
-            const articlesArr = data.articles
+		if (filter.length > 0) {
+			console.log("did this happen");
 			res.status(201);
 			res.type("text/html");
 			res.render("index", {
-                articlesArr
-            });
+				searchState: JSON.stringify(prevSearches),
+				articlesArr: filter[0].articlesArr,
+			});
+		} else {
+			return next();
+		}
+	},
+	async (req, res) => {
+		console.log("we are fetching");
+		const fetchUrl = withQuery(NEWS_URL, {
+			q: req.body.search,
+			/* apiKey: `${process.env.API_KEY}`, */
+			country: req.body.country,
+			category: req.body.category,
+		});
+		const prevSearches = JSON.parse(req.body.searchState);
+
+		try {
+			const result = await fetch(fetchUrl, { headers });
+			const data = await result.json();
+
+			const articlesArr = data.articles;
+			console.log(data);
+			prevSearches.push({
+				q: req.body.search,
+				country: req.body.country,
+				category: req.body.category,
+				fetchUrl,
+				articlesArr,
+			});
+			res.status(201);
+			res.type("text/html");
+			console.log("we are rendering");
+			res.render("index", {
+				articlesArr,
+				searchState: JSON.stringify(prevSearches),
+			});
+			/*   setTimeout(()=> {
+                console.log("too bad its gonan be deleted")
+                 res.render("index", {
+                     articlesArr,
+                     searchState: "[]"
+                 })
+    
+            },1000) */
 		} catch (e) {
 			console.log(e);
 		}
